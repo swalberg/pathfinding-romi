7723929ab1e6b6556ebeeeb1b1ce7172699f1237
Sean <sean@ertw.com>
2025-03-25 13:26:33 -0400

    add a robotcontainer test

---
+++ src/test/java/frc/robot/RobotContainerTest.java
@@ -0,0 +1,17 @@
+package frc.robot;
+import static org.junit.jupiter.api.Assertions.fail;
+import org.junit.jupiter.api.Test;
+
+public class RobotContainerTest {
+
+  @Test
+  public void createRobotContainer() {
+    // Instantiate RobotContainer
+    try {
+      new RobotContainer();
+    } catch (Exception e) {
+      e.printStackTrace();
+      fail("Failed to instantiate RobotContainer, see stack trace above.");
+    }
+  }
+}
